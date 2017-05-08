e671cc88dfafa06f8ad970b0f72c5d12dfc307f6
Edward Vidal Jr <develone@sbcglobal.net>
2017-05-08 14:10:07 +0000

    Made chgs per recommendation by Ultibo You seem to have the Pascal declarations of the functions pretty much correct, in order to allow them to be called by the C code they need to be made public and also need to be declared with the C calling convention.
    To support calling from C it should look like this:
    
    function mem_alloc(file_desc:Integer; size, align, flags:Longword):Longword; cdecl; public name 'mem_alloc';

--- host_applications/linux/apps/hello_pi/hello_fft/umailbox.pas
+++ host_applications/linux/apps/hello_pi/hello_fft/umailbox.pas
@@ -9,95 +9,46 @@ interface
 
 uses GlobalConfig,GlobalConst,GlobalTypes,Platform;
 
-function mem_alloc(file_desc:Integer; size, align, flags:Longword):Longword;
-function mem_free(file_desc:Integer;  handle:Longword):Longword;
-function mem_lock(file_desc:Integer;  handle:Longword):Longword;
-function mbox_open():Integer;
-function execute_code(file_desc:Integer; r0, r1, r2, r3, r4, r5:Longword):Longword;
+function mem_alloc(file_desc:Integer; size, align, flags:Longword):Longword; cdecl; public name 'mem_alloc';
+function mem_free(file_desc:Integer;  handle:Longword):Longword; cdecl; public name 'mem_free';
+function mem_lock( handle:Longword):Longword; cdecl; public name 'mem_lock';
+function mbox_open():Integer; cdecl; public name 'mbox_open';
+function execute_code(file_desc:Pointer; r0, r1, r2, r3, r4, r5:Longword):Longword; cdecl; public name 'execute_code';
 
 implementation
  
-function mem_alloc(file_desc:Integer; size, align, flags:Longword):Longword;
+function mem_alloc(file_desc:Integer; size, align, flags:Longword):Longword; cdecl;
 
-	var
-	p : array[0..31] of Longword;
+ 
 
 	begin
-	
- 
- 	p[0] := 0;
-	p[1] := $00000000;
-	p[2] := $3000c;//196620 mem_alloc
- 
-	p[3] := 12; //mem_alloc
-	p[4] := 12; //mem_alloc
- 
-	Result := p[5];
+	Result := GPUMemoryAllocate(size, align, flags);
 	end;
 	
 
-function mem_free(file_desc:Integer;  handle:Longword):Longword;
-
-	var
-	p : array[0..31] of Longword;
+function mem_free(file_desc:Integer;  handle:Longword):Longword; cdecl;
 
 	begin
-	
- 
- 	p[0] := 0;
-	p[1] := $00000000;
-	p[2] := $3000f;//196623 mem_free
- 	
- 
-	p[3] := 4; //mem_free
-	p[4] := 4; //mem_free
-    p[5] := handle; 
-	Result := p[5];
+	Result := GPUMemoryRelease(handle);
 	end;
 
-function mem_lock(file_desc:Integer;  handle:Longword):Longword;
-
-	var
-	p : array[0..31] of Longword;
-
+function mem_lock(handle:Longword):Longword; cdecl; 
 	begin
-	
- 
- 	p[0] := 0;
-	p[1] := $00000000;
-	p[2] := $3000d;//196623 mem_free
- 	
- 
-	p[3] := 4; //mem_free
-	p[4] := 4; //mem_free
-    p[5] := handle; 
-	Result := p[5];
+	Result := GPUMemoryLock(handle);
 	end;
 
-function mbox_open():Integer;
+function mbox_open():Integer; cdecl;
 	var
 	file_desc:Integer;
 	begin
 	Result := file_desc;
 	end;
 	
-function execute_code(file_desc:Integer; r0, r1, r2, r3, r4, r5:Longword):Longword;
-	var
-	p : array[0..31] of Longword;
+function execute_code(file_desc:Pointer; r0, r1, r2, r3, r4, r5:Longword):Longword; cdecl;
+  
+    
 
 	begin
-	p[0] := 0;
-	p[1] := $00000000;
-	p[2] := $30010;
-	p[3] := 28; //execute_code
-	p[4] := 28; //execute_code
-	p[6] := r0;
-    p[7] := r1;
-    p[8] := r2;
-    p[9] := r3;
-    p[10] := r4;
-    p[11] := r5;
-    p[12] := $00000000; // end tag
-	Result := p[5];
+	Result := GPUExecuteCode(file_desc,R0,R1,R2,R3,R4,R5);
 	end;
 end.
