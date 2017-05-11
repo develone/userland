dc2dac62122de1dbcac18ec1eefd81aee9b5d3db
Edward Vidal Jr <develone@sbcglobal.net>
2017-05-11 15:46:10 +0000

    function gpu_fft_get_host_info(info : PGPU_FFT_HOST):Longword; cdecl; begin    info^.peri_addr := PeripheralGetBase;    info^.peri_size := PeripheralGetsize + 1;
       //Add the rest of the implementation here
    
       Result := 0;
    end;

---
+++ host_applications/linux/apps/hello_pi/hello_fft/uGPU_FFT_HOST.pas
@@ -0,0 +1,41 @@
+
+{$mode delphi} {Default to Delphi compatible syntax}
+{$H+}          {Default to AnsiString}
+{$inline on}   {Allow use of Inline procedures}
+
+unit uGPU_FFT_HOST;
+ 
+interface
+
+uses GlobalConfig,GlobalConst,GlobalTypes,Platform;
+
+ 
+type
+PGPU_FFT_HOST = ^GPU_FFT_HOST;
+GPU_FFT_HOST = record
+	mem_flg, mem_map, peri_addr, peri_size : Longword;
+end;
+
+var
+	GPU_FFT_HOST_list : GPU_FFT_HOST;
+
+type 
+	GPU_FFT_HOST_listPointer = ^GPU_FFT_HOST;
+   
+function gpu_fft_get_host_info(info : PGPU_FFT_HOST):Longword; cdecl; public name 'gpu_fft_get_host_info';
+  
+
+
+implementation
+function gpu_fft_get_host_info(info : PGPU_FFT_HOST):Longword; cdecl;
+begin
+   info^.peri_addr := PeripheralGetBase;
+   info^.peri_size := PeripheralGetsize + 1;
+
+   //Add the rest of the implementation here
+   
+   Result := 0;
+end;
+ 
+ 	
+ end.
